f5a56301ab51e0709cce9a53e6768aba8f9bbdf2
Radoslav Elias <radoslave0@gmail.com>
2023-11-19 11:12:12 +0000

    v2.0: added sorting, filtering by activity type

--- src/parse.py
+++ src/parse.py
@@ -1,17 +1,39 @@
 import load_activities
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawTextHelpFormatter
+from enum import Enum
 
-from rich import print as rprint
 from rich.console import Console
 
-
 console = Console()
+
+
+class ActivityType(Enum):
+    run = 'Run'
+    ride = 'Ride'
+    hike = 'Hike'
+    workout = 'Workout'
+    rock_climbing = 'RockClimbing'
+    nordic_ski = 'NordicSki'
+    alpine_ski = 'AlpineSki'
+
+    def __str__(self):
+        return self.value
+    
+
+class Attribute(Enum):
+    distance = 'distance'
+    elevation_gain = 'total_elevation_gain'
+    avg_heartrate = 'average_heartrate'
+
+    def __str__(self):
+        return self.value
 
 
 def pretty_print(data):
     print(f"Total acitivities: {len(data)}")
     for activity in data:
-        console.print(f"[bold][red]{activity['name']}: [blue]https://strava.com/activities/{activity['id']}")
+        console.print(f"[bold][red]{activity['name']}: \
+[blue]https://strava.com/activities/{activity['id']}")
         attributes = f"  -  [bold]distance[/bold]: "
         attributes += f"[green]{round(float(activity['distance'])/1000, 2)}km[/green], "
         attributes += f"[bold]elevation gain[/bold]: "
@@ -24,7 +46,7 @@ def pretty_print(data):
         console.print(attributes, highlight=False)
     
     
-def validate_filters(filters):
+def validate_attr_filters(filters):
     filters = filters.split(' ')
     if len(filters) % 2 != 0:
         raise Exception("Filters are expected to come in pairs of symbols and values.")
@@ -41,7 +63,7 @@ def validate_filters(filters):
     return parsed_filters
 
 
-def apply_filters(data, attribute, filters):
+def apply_attr_filters(data, attribute, filters):
     filtered_data = []
     for filter in filters:
         for activity in data:
@@ -53,19 +75,54 @@ def apply_filters(data, attribute, filters):
     return filtered_data
 
 
+def filter_activity_types(data, types):
+    type_filters = [f.value for f in types]
+    data = [activity for activity in data if activity['type'] in type_filters]
+    return data
+
+
+def sort_by_attr(data, sort_arg):
+    attribute, order = sort_arg.split(':')
+    try: 
+        attribute = Attribute[attribute].value
+    except KeyError:
+        raise Exception(f"Incorrect attribute specified in sortby: {attribute}")
+    reverse_order = (order.lower() == 'desc')
+    data = sorted(data, key=lambda x: x[attribute], reverse=reverse_order)
+    return data
+
+
 if __name__ == '__main__':
-    argparser = ArgumentParser(description="Filter strava activities by your parameters.")
+    argparser = ArgumentParser(description=f"""Filter strava activities by your parameters.
+Available attributes to filter by: {list(Attribute.__members__)}""",
+                               formatter_class=RawTextHelpFormatter)
+    argparser.add_argument('--type', type=ActivityType, choices=list(ActivityType), 
+                           help='filter by specific activity type', nargs='*', action='extend')
     argparser.add_argument('-d', '--distance', type=str, help='set the distance filters[m]')
-    argparser.add_argument('-eg', '--total_elevation_gain', type=str, help='set the elevation gain filter[m]')
-    argparser.add_argument('-hr', '--average_heartrate', type=str, help='set the average heartrate filter[bpm]')
+    argparser.add_argument('-eg', '--elevation_gain', type=str, 
+                           help='set the elevation gain filter[m]')
+    argparser.add_argument('-hr', '--avg_heartrate', type=str, 
+                           help='set the average heartrate filter[bpm]')
+    argparser.add_argument('--sortby', type=str, 
+                           help="Sort by specific attribute and order: 'attribute_name:[desc/asc]'")
 
     args = argparser.parse_args()
 
     data = load_activities.load()
+
+    if args.type:
+        data = filter_activity_types(data, args.type)
+
     for attribute, filters in vars(args).items():
-        if filters is None:
+        # skip if filter not specified or not attribute filter [dist, elev, hr]
+        if filters is None or attribute not in Attribute.__members__:
             continue
-        filters = validate_filters(filters)
-        data = apply_filters(data, attribute, filters)
+        attribute = Attribute[attribute].value
+        filters = validate_attr_filters(filters)
+        data = apply_attr_filters(data, attribute, filters)
+    
+    if args.sortby:
+        data = sort_by_attr(data, args.sortby)
+        
     
     pretty_print(data)
